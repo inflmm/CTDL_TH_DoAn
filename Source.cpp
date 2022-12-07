0d61ff02ce05f985a7069640f6ff976b61d2198f
inflmm <94544986+inflmm@users.noreply.github.com>
2022-12-07 18:10:02 +0700

    Thiết kế lại
    Có kèm theo file csv 1000 dòng để chạy thử, làm xong phần đọc và tách file rồi, còn merge lại nữa

--- Source.cpp
+++ Source.cpp
@@ -2,94 +2,86 @@
 #include<fstream>
 #include <vector>
 #include<string>
-#include <chrono>
-// Dong dau tien trong file khong can sap xep, ghi thang vao
-// NRow: so luong dong doc 1 lan (RAM 4GB: 800,000 dong, RAM 8GB: 2,000,000 dong)
-using namespace std::chrono;
-using namespace std;
-struct Book {
-    string BookId;
-    string title;
-    string price;
-    string userId;
-    string profileName;
-    string reviewHelpful;
-    string reviewScore;
-    string reviewTime;
-    string reviewSummary;
-    string reviewText;
-};
 
-struct List
+//Làm lại
+// 
+// Dong dau tien trong file khong can sap xep, ghi thang vao
+// NRow: so luong dong doc 1 lan (RAM 4GB: 300,000 dong, RAM 8GB: 2,000,000 dong)
+
+using namespace std;
+
+struct Book
 {
     string ID;
-    string line;
+    string text;
 };
-vector<List> LinkedList;
-vector<Book> Listbook;
-void WriteElementOfBook(Book& book, string x, int count)
+int RowLimit = 250; // 4 GB RAM: 300000, 8 GB RAM: 2000000
+
+int partition(Book*& arr, int low, int high)
 {
-    switch (count)
-    {
-    case(0):
-    {
-        book.BookId = x;
-        break;
+    string pivot = arr[high].ID;
+    int left = low;
+    int right = high - 1;
+    while (true) {
+        while (left <= right && arr[left].ID < pivot) left++;
+        while (right >= left && arr[right].ID > pivot) right--;
+        if (left >= right) break;
+        swap(arr[left], arr[right]);
+        left++;
+        right--;
     }
-    case(1):
+    swap(arr[left], arr[high]);
+    return left;
+}
+
+/* Hàm thực hiện giải thuật quick sort */
+void quickSort(Book*& arr, int low, int high)
+{
+    if (low < high)
     {
-        book.title = x;
-        break;
-    }
-    case(2):
-    {
-        book.price = x;
-        break;
-    }
-    case(3):
-    {
-        book.userId = x;
-        break;
-    }
-    case(4):
-    {
-        book.profileName = x;
-        break;
-    }
-    case(5):
-    {
-        book.reviewHelpful = x;
-        break;
-    }
-    case(6):
-    {
-        book.reviewScore = x;
-        break;
-    }
-    case(7):
-    {
-        book.reviewTime = x;
-        break;
-    }
-    case(8):
-    {
-        book.reviewSummary = x;
-        break;
-    }
-    default:
-        book.reviewText = x;
-        break;
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
     }
 }
-void readFileOnly(vector<List>& LinkedList, int start, int end)
+
+void writeSmallFile(Book*& Lines, int& end, vector<string>& Sorted, int& count)
 {
-    //string fileName("test.txt");
-    string fileName("Books_rating.csv");
-    ifstream file(fileName, ios_base::in | ios_base::binary);
-    string line;
+    string name;
+    name = "Sorted";
+    name = name + to_string(count) + ".csv";
+    count++;
+    Sorted.push_back(name);
+
+    fstream file(name, ios_base::out);
     if (!file.is_open())
     {
-        cout << "Error" << endl;
+        cout << "Cannot open file." << endl;
+    }
+    else
+    {
+        int i = 0;
+        while (i < end)
+        {
+            file << Lines[i].text << endl;
+            i++;
+        }
+    }
+    file.close();
+}
+
+void ReadAndCreateFile(vector<string> Sorted, vector<string> Merge)
+{
+    //int limit = 1000;
+
+    string line;
+    string fileName("Books_rating_test.csv");
+    int count = 0;
+    
+    fstream file(fileName, ios_base::in);
+    if (!file.is_open())
+    {
+        cout << "Cannot open file." << endl;
     }
     else
     {
@@ -99,167 +91,109 @@ void readFileOnly(vector<List>& LinkedList, int start, int end)
             int length = line.length() + 1;
             file.seekg(length);
         }
-        for (int i = start; i < end; i++)
+        while (!file.eof())
         {
-            getline(file, line, '\n');
-            List L;
-            string s;
-            L.line = line;
-            int length = line.length();
-            for (int j = 0; j < length; j++)
+            Book* Lines = new Book[RowLimit];
+            int i = 0;
+            while (i < RowLimit)
             {
-                if (line[j] == ',')
+                if (file.eof())
                 {
                     break;
                 }
-                s += line[j];
-            }
-            L.ID = s;
-            LinkedList.push_back(L);
-        }
-        /*for (int i = 0; i < end-1; i++)
-        {
-            cout << LinkedList[i].ID << "####" << LinkedList[i].line << endl;
-        }*/
-        
-    }
-    
-    file.close();
-}
-void writeFile(vector<List> LinkedList, int start, int end)
-{
-    int count = 0;
-    string fileName("Books_rating_Sorted.csv");
-    fstream file(fileName, ios_base::out);
-    if (!file.is_open())
-    {
-        cout << "Cannot open file." << endl;
-    }
-    else
-    {
-        while (count < end)
-        {
-            file << LinkedList[start + count].line << endl;
-            count++;
-        }
-    }
-    file.close();
-}
-void Run()
-{
-    vector<List> LinkedList;
-    vector<string> lines;
-    int Nrow = 10;
-    readFileOnly(LinkedList, 0, 100000);
-   // writeFile(LinkedList, 0, 10);
-}
-void printBook(Book book)
-{
-    cout << " IDBook:  " << book.BookId << endl;
-    cout << " title:  " << book.title << endl;
-    cout << " price:  " << book.price << endl;
-    cout << " user_id:  " << book.userId << endl;
-    cout << " profileName:  " << book.profileName << endl;
-    cout << " reviewHelpful:  " << book.reviewHelpful << endl;
-    cout << " reviewScore:  " << book.reviewScore << endl;
-    cout << " reviewTime:  " << book.reviewTime << endl;
-    cout << " reviewSummary:  " << book.reviewSummary << endl;
-    cout << " reviewText:  " << book.reviewText << endl;
-}
-void printListBook(vector<Book> Listbook)
-{
-    for (int i = 0; i < Listbook.size(); i++)
-    {
-        printBook(Listbook[i]);
-        cout << endl;
-    }
-}
-void ReadFile(vector<Book>& Listbook, int NRow)
-{
-    string filename("Book2.csv");
-    // Khai báo vector để lưu các dòng đọc được
-    vector<string> lines;
-    string line;
-
-    //Mở file bằng ifstream
-    ifstream input_file(filename, ios_base::in | ios_base::binary);
-    //Kiểm tra file đã mở thành công chưa
-    if (!input_file.is_open()) {
-        cerr << "Could not open the file - '"
-            << filename << "'" << endl;
-        return;
-    }
-
-    //Đọc từng dòng trong
-    while (getline(input_file, line, '\n')) {
-        lines.push_back(line);//Lưu từng dòng như một phần tử vào vector lines.
-
-    }
-    //Xuất từng dòng từ lines và in ra màn hình
-    int length = lines.size();
-    for (int i = 0; i < length; i++)
-    {
-        //cout << lines[i] << endl;
-        Book book;
-        string x;
-        int count = 0;
-        int lengthLine = lines[i].length();
-        //de nhan biet bat dau mot chuoi co dau phay hay khong
-        bool flag = false;
-        for (int j = 0; j < lengthLine; j++)
-        {
-            if (j == lengthLine - 2)
-            {
-                WriteElementOfBook(book, x, count);
-                x = "";
-                break;
-            }
-            else if (lines[i][j] == ',' && flag == false)
-            {
-                WriteElementOfBook(book, x, count);
-                count++;
-                x = "";
-            }
-            else if (lines[i][j] == '"' && flag == false)
-            {
-                flag = true;
-            }
-            else if (lines[i][j] == '"' && flag == true)
-            {
-
-                if (lines[i][j + 1] == '"')
+                getline(file, line, '\n');
+                string s = "";
+                int length = line.length();
+                for (int j = 0; j < length; j++)
                 {
-                    x += '"';
-                    j++;
+                    if (line[j] == ',')
+                    {
+                        break;
+                    }
+                    s += line[j];
                 }
-                else if (lines[i][j + 1] == ',')
+                Lines[i].ID = s;
+                Lines[i].text = line;
+                i++;
+            }
+            
+
+            quickSort(Lines, 0, i - 1);
+
+            //trong trường hợp file nhỏ mà ram có thể chứa hết các dòng, đến end of file, thì cho sắp xếp rồi xuất thẳng file ra, nhớ delete Lines
+            if (file.eof() == true && count == 0) // nghĩa là đã đến cuối file nhưng có ghi file nhỏ nào cả
+            {
+                //cout << "true" << endl;
+                fstream fileResult("Books_rating_Sorted.csv", ios_base::out);
+                if (!fileResult.is_open())
                 {
-                    WriteElementOfBook(book, x, count);
-                    count++;
-                    x = "";
-                    flag = false;
+                    cout << "Cannot open file." << endl;
                 }
                 else
                 {
-                    x += lines[i][j];
+                    file.close();
+                    file.open(fileName, ios_base::in);
+                    //file.seekg(0, file.beg);
+                    getline(file, line);
+                    fileResult << line;
+                    cout << line << endl;
+                    cout << Lines[0].text << endl;
+                    cout << Lines[1].text << endl;
+                    cout << Lines[2].text << endl;
+                    int j = 0;
+                    while (j < i - 1)
+                    {
+                        //cout << Lines[j].text << endl;
+                        fileResult << Lines[j].text << endl;
+                        j++;
+                    }
+                    cout << "The file has been sorted successfully." << endl;
                 }
+                fileResult.close();
+                file.close();
+                delete[] Lines;
+
+                
+
+                return;
             }
-            else
-            {
-                x += lines[i][j];
-            }
+
+            writeSmallFile(Lines, i, Sorted, count);
+
+            delete[] Lines;
         }
-        Listbook.push_back(book);
+        //Khởi tạo Merge file
+        for (int i = 0; i < count - 1; i++)
+        {
+            string name = "Merge";
+            name = name + to_string(i) + ".csv";
+            fstream mergeFile(name, ios::out);
+            mergeFile.close();
+        }
+
+
     }
-    //printListBook(Listbook);
-    //Đóng file
-    input_file.close();
+    file.close();
+}
+
+void MergeFile(vector<string> Sorted, vector<string> Merge)
+{
+
+}
+
+void Run()
+{
+    vector<string> Sorted;
+    vector<string> Merge;
+
+    ReadAndCreateFile(Sorted, Merge);
+    MergeFile(Sorted, Merge);
 }
 
 int main()
 {
-    //Run();
-    //readFileOnly();
-    //ReadFile();
+    Run();
+
     return 0;
 }
+
